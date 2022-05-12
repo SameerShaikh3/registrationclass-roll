6acc37349cfd8eb4e9ad3853e63fa7a004352430
sameer <sameerart@gmail.com>
2022-05-12 17:51:52 +0530

    Added more python

---
+++ sam.py
@@ -0,0 +1,129 @@
+print("total 50 roll are stored can stored in this program")
+c = int(input("enter your class : "))
+i=int(input("enter roll no : "))
+if c == 1:
+    print(i)
+elif c == 2:
+    print(i)
+elif c == 3:
+    print(i)
+elif c == 4:
+    print(i)
+elif c == 5:
+    print(i)
+elif c == 6:
+    print(i)
+elif c == 7:
+    print(i)
+elif c == 8:
+    print(i)
+elif c == 9:
+    print(i)
+elif c == 10:
+    print(i)
+else: 
+    print ("please enter valid class 1 to 10th")
+if i ==0:
+    print("enter valid roll exept 0")
+elif i == 1 :
+    tr = input("enter name : ")
+elif i == 2 :
+    tu = input("enter name : ")
+elif i == 3 :
+    ti = input("enter name : ")
+elif i == 4 :
+    tp = input("enter name : ")
+elif i == 5 :
+    tt = input("enter name : ")
+elif i == 6 :
+    to = input("enter name : ")
+elif i == 7 :
+    tp = input("enter name : ")
+elif i == 8 :
+    tv = input("enter name : ")
+elif i == 9 :
+    tf = input("enter name : ")
+elif i == 10 :
+    te = input("enter name : ")
+elif i == 11 :
+    tr = input("enter name : ")
+elif i == 12 :
+    tu = input("enter name : ")
+elif i == 13 :
+    ti = input("enter name : ")
+elif i == 14 :
+    tp = input("enter name : ")
+elif i == 15 :
+    tt = input("enter name : ")
+elif i == 16 :
+    to = input("enter name : ")
+elif i == 17 :
+    tp = input("enter name : ")
+elif i == 18 :
+    tv = input("enter name : ")
+elif i == 19 :
+    tf = input("enter name : ")
+elif i == 20 :
+    te = input("enter name : ")
+elif i == 21 :
+    tr = input("enter name : ")
+elif i == 22 :
+    tu = input("enter name : ")
+elif i == 23 :
+    ti = input("enter name : ")
+elif i == 24 :
+    tp = input("enter name : ")
+elif i == 25 :
+    tt = input("enter name : ")
+elif i == 26 :
+    to = input("enter name : ")
+elif i == 27 :
+    tp = input("enter name : ")
+elif i == 28 :
+    tv = input("enter name : ")
+elif i == 29 :
+    tf = input("enter name : ")
+elif i == 30 :
+    te = input("enter name : ")
+elif i == 31 :
+    tr = input("enter name : ")
+elif i == 32 :
+    tu = input("enter name : ")
+elif i == 33 :
+    ti = input("enter name : ")
+elif i == 34 :
+    tp = input("enter name : ")
+elif i == 35 :
+    tt = input("enter name : ")
+elif i == 36 :
+    to = input("enter name : ")
+elif i == 37 :
+    tp = input("enter name : ")
+elif i == 38 :
+    tv = input("enter name : ")
+elif i == 39 :
+    tf = input("enter name : ")
+elif i == 40 :
+    te = input("enter name : ")
+elif i == 41 :
+    tr = input("enter name : ")
+elif i == 42 :
+    tu = input("enter name : ")
+elif i == 43 :
+    ti = input("enter name : ")
+elif i == 44 :
+    tp = input("enter name : ")
+elif i == 45 :
+    tt = input("enter name : ")
+elif i == 46 :
+    to = input("enter name : ")
+elif i == 47 :
+    tp = input("enter name : ")
+elif i == 48 :
+    tv = input("enter name : ")
+elif i == 49 :
+    tf = input("enter name : ")
+elif i == 50 :
+    te = input("enter name : ")
+else:
+    print("type less than 50 roll number")
